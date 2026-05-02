f1c88ad9dab6ac7b7081d2a0db580dfcddb76bee
Juan Victor <273195986+JuanVictorFY@users.noreply.github.com>
2026-05-02 00:01:27 -0500

    feat: crear pagina de nosotros y configurar enrutamiento global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Servicios } from './pages/servicios/servicios';
+import { Nosotros } from './pages/nosotros/nosotros';
 
 export const routes: Routes = [
   { path: '', component: Home },               // Ruta base (Inicio)
   { path: 'servicios', component: Servicios }, // Tu nueva página
+  { path: 'nosotros', component: Nosotros },   // Página de Nosotros
   { path: '**', redirectTo: '' }               // Si la URL no existe, vuelve al inicio
 ];
