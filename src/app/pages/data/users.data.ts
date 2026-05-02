e14622d16c2221299a42c420e797c3204ba17abe
Juan Victor <273195986+JuanVictorFY@users.noreply.github.com>
2026-05-02 12:13:03 -0500

    feat(login): separar credenciales en archivo data y agregar validacion con redireccion a intranet

---
+++ src/app/pages/data/users.data.ts
@@ -0,0 +1,7 @@
+// Usuarios temporales (Mocks) para pruebas de inicio de sesión
+export const USERS_MOCK = [
+  { email: 'admin@dentalpro.com', password: '123', role: 'Administrador' },
+  { email: 'recepcion@dentalpro.com', password: '123', role: 'Recepcionista' },
+  { email: 'odontologo@dentalpro.com', password: '123', role: 'Odontólogo' },
+  { email: 'paciente@gmail.com', password: '123', role: 'Paciente' }
+];
