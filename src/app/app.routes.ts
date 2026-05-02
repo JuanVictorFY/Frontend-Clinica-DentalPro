1e2679e41a42c1b524573ce16abe6f56ad9bae7b
Juan Victor <273195986+JuanVictorFY@users.noreply.github.com>
2026-05-01 22:36:31 -0500

    feat: crear página de servicios y configurar enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Servicios } from './pages/servicios/servicios';
 
 export const routes: Routes = [
-  { path: '', component: Home }, // Esto le dice a Angular: "En el inicio, carga Home"
+  { path: '', component: Home },               // Ruta base (Inicio)
+  { path: 'servicios', component: Servicios }, // Tu nueva página
+  { path: '**', redirectTo: '' }               // Si la URL no existe, vuelve al inicio
 ];
