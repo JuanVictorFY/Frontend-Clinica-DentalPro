039fc2dc80054fa8bd624c0714d4da9f9cf3fd44
Juan Victor <figyuptonj99@gmail.com>
2026-05-02 12:12:57 -0500

    feat(intranet): crear vista de intranet en construccion y configurar enrutamiento

---
+++ src/app/pages/intranet/intranet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-intranet',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './intranet.html',
+  styleUrl: './intranet.css',
+})
+export class Intranet {}
