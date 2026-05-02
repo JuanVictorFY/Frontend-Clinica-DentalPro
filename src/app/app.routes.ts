7fa8666adade1e654b69bdb8b359e4c043fd9d9c
Juan Victor <273195986+JuanVictorFY@users.noreply.github.com>
2026-05-02 12:12:57 -0500

    feat(intranet): crear vista de intranet en construccion y configurar enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { Servicios } from './pages/servicios/servicios';
 import { Nosotros } from './pages/nosotros/nosotros';
 import { Login } from './pages/login/login';
+import { Intranet } from './pages/intranet/intranet';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' }, // Ruta base (Inicio)
   { path: 'servicios', component: Servicios }, // Tu nueva página
   { path: 'nosotros', component: Nosotros },   // Página de Nosotros
   { path: 'login', component: Login },         // Página de Login
+  { path: 'intranet', component: Intranet },   // Página de Intranet
   { path: '**', redirectTo: '' }               // Si la URL no existe, vuelve al inicio
 ];
