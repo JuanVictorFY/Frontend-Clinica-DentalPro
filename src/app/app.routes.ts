eb33aab79f10f01ade643222b51ef108f8f5903c
Juan Victor <figyuptonj99@gmail.com>
2026-05-02 01:45:24 -0500

    fix(routes): corregir importacion de login y agregar pathMatch full a la ruta de inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Servicios } from './pages/servicios/servicios';
 import { Nosotros } from './pages/nosotros/nosotros';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
-  { path: '', component: Home },               // Ruta base (Inicio)
+  { path: '', component: Home, pathMatch: 'full' }, // Ruta base (Inicio)
   { path: 'servicios', component: Servicios }, // Tu nueva página
   { path: 'nosotros', component: Nosotros },   // Página de Nosotros
+  { path: 'login', component: Login },         // Página de Login
   { path: '**', redirectTo: '' }               // Si la URL no existe, vuelve al inicio
 ];
